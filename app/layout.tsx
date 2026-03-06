68b6e9786651719b8509e35587a2196756560b0d
Mandar <smand@mandar>
2026-03-06 17:51:34 +0530

    SEO & GTM Final Optimization: next-sitemap setup, homepage schema, and descriptive alts

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,13 +95,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${garamond.variable}`}>
       <head>
-        <script
+        <Script
+          id="gtm-base"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5DKSD76W');`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5DKSD76W');`,
           }}
         />
         {/* DNS Prefetch for external resources */}
